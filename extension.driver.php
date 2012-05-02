6c98c222b9a8299101d272e756a71db617ba856f
Jens Scherbl <jens@scherbl.net>
2012-05-02 04:13:27 +0200

    Compatible with Symphony 2.3

--- extension.driver.php
+++ extension.driver.php
@@ -1,24 +1,6 @@
 <?php
 
-	Class extension_breadcrumb extends Extension{
+	Class extension_breadcrumb extends Extension {
 
-		public function about(){
-			return array(
-				'name' => 'Breadcrumb',
-				'version' => '1.1',
-				'release-date' => '2011-07-03',
-				'author' => array(
-					array(
-						'name' => 'Alistair Kearney',
-						'website' => 'http://pointybeard.com',
-						'email' => 'alistair@pointybeard.com'
-					),
-					array(
-						'name' => 'Solutions Nitriques',
-						'website' => 'http://nitriques.com',
-						'email' => 'open-source[at]nitriques.com'
-					)
-				)
-			);
-		}
+
 	}
